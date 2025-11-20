38fea72403ece40d9d9bab55aed1ac85a4243595
Josh Cunningham <josh@joshcanhelp.com>
2025-11-19 17:23:57 -0800

    fix: adjust models and migrations

--- api/models.py
+++ api/models.py
@@ -3,10 +3,9 @@ from django.conf import settings
 import uuid
 
 
-class Vaults(models.Model):
+class Vault(models.Model):
     id = models.UUIDField("Vault ID", default=uuid.uuid4, primary_key=True)
     name = models.CharField("Vault name", max_length=200)
-    api_key = models.CharField("API key", max_length=64)
     is_active = models.BooleanField("Is this vault active?", default=True)
     created_utc_s = models.DateTimeField(
         "Date/time for when the Vault was created",
@@ -15,7 +14,7 @@ class Vaults(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
-class ApiKeys(models.Model):
+class ApiKey(models.Model):
     key = models.CharField("API key", max_length=64, unique=True, primary_key=True)
     created_utc_s = models.DateTimeField(
         "Date/time for when the API key was created",
@@ -26,14 +25,14 @@ class ApiKeys(models.Model):
         auto_now=True,
     )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    vault = models.ForeignKey(Vaults, on_delete=models.CASCADE)
+    vault = models.ForeignKey(Vault, on_delete=models.CASCADE)
 
 
-class Actions(models.Model):
+class Action(models.Model):
     id = models.UUIDField("Action ID", default=uuid.uuid4, primary_key=True)
     type = models.CharField("Action type", max_length=20)
     status = models.CharField("Status of the action", max_length=20)
-    path = models.CharField("ath to the file in the vault", max_length=300)
+    path = models.CharField("Path to the file in the vault", max_length=300)
     created_utc_s = models.DateTimeField(
         "Date/time when the Action was created",
         auto_now=True,
@@ -42,4 +41,4 @@ class Actions(models.Model):
         "Date/time when the Action was completed by the client",
         auto_now=True,
     )
-    vault = models.ForeignKey(Vaults, on_delete=models.CASCADE)
+    vault = models.ForeignKey(Vault, on_delete=models.CASCADE)
