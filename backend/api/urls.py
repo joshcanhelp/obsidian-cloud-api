0e846d8f5cf7cf1d0f112da87b4cd43d40172646
Josh Cunningham <josh@joshcanhelp.com>
2025-11-20 16:45:41 -0800

    feat: format JSON output, register route

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.root, name="root"),
+    path("register", views.userRegistration, name="userRegistration"),
     path("vaults", views.vaultList, name="vaultList"),
     path("keys", views.keyList, name="keyList"),
     path("actions", views.actionList, name="actionList"),
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,22 @@
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 from .models import Vault, ApiKey, Action
 
 from django.utils import timezone
 
+# TODO: Switch this to class-based views
+# https://docs.djangoproject.com/en/dev/topics/class-based-views/
 
 def root(request):
-    return JsonResponse({})
+    return JsonResponse({"hi": True})
+
+def userRegistration(request):
+    user_list = list(User.objects.all().values())
+    # I wonder if I can extend JsonReponse for all the output boilerplate ...
+    return JsonResponse(user_list, safe=False, json_dumps_params={"indent": 2})
 
 
+# Views that need authentication
 def vaultList(request):
     all_vaults = Vault.objects.filter(is_active__exact=True)
     vault_list = list(all_vaults.values("id", "name", "created_utc_s", "user_id"))
@@ -29,7 +38,7 @@ def keyList(request):
                 "key": orig_key["key"][:3] + "***",
             }
         )
-    return JsonResponse(return_list, safe=False)
+    return JsonResponse(return_list, safe=False, json_dumps_params={"indent": 2})
 
 def actionList(request):
     all_actions = Action.objects.order_by("created_utc_s").values(
@@ -38,4 +47,4 @@ def actionList(request):
     action_list = list(
         all_actions.values("type", "vault", "created_utc_s", "status", "path")
     )
-    return JsonResponse(action_list, safe=False)
+    return JsonResponse(action_list, safe=False, json_dumps_params={"indent": 2})
